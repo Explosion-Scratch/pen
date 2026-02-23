ccd425e11d077c7cd973b85a7090debe3bb32d97
Explosion Implosion <explosionscratch@gmail.com>
2026-02-22 22:09:21 -0500

    Ugh

--- test-less.js
+++
@@ -1,7 +0,0 @@
-import less from 'less';
-const code = `
-#app {
-  @apply flex h-screen justify-center items-center;
-}
-`;
-less.render(code).then(res => console.log(res.css));
--- test-sass.js
+++
@@ -1,7 +0,0 @@
-const sass = require('sass');
-const code = `
-#app {
-  @apply flex h-screen justify-center items-center;
-}
-`;
-console.log(sass.compileString(code).css);
